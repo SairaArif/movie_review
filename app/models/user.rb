00b8d11228527f43431ffd90b389c95e170ab9ff
SairaArif <saira.ariff@gmail.com>
2019-05-05 21:48:46 +0500

    Added name field to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable,  :validatable
    has_many :movies
    has_many :reviews, dependent: :destroy
+
+  validates :email,
+              :presence => true,
+              :uniqueness => {
+                  :case_sensitive => false
+              }
+  validates :name,
+              :presence => true
 end
